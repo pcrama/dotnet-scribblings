7240816e3c8ef0cc59052e96577dda8fc3c46fc3
Philippe Crama <pcfeb0009@gmx.com>
2020-03-10 21:53:03 +0100

    Change data model to play a bit

--- xmlsetcmp/Program.cs
+++ xmlsetcmp/Program.cs
@@ -12,8 +12,7 @@ using System.Xml.Serialization;
 // the attribute sets the IsNullable property, which specifies whether
 // the xsi:null attribute appears if the class instance is set to
 // a null reference.
-[XmlRoot("PurchaseOrder", Namespace="http://www.cpandl.com",
-IsNullable = false)]
+[XmlRoot("PurchaseOrder", Namespace=null, IsNullable = false)]
 public class PurchaseOrder
 {
     public Address ShipTo;
@@ -25,6 +24,21 @@ public class PurchaseOrder
     public decimal SubTotal;
     public decimal ShipCost;
     public decimal TotalCost;
+    public int ItemCount;
+
+    public void Calculate(decimal shipCost)
+    {
+        ItemCount = OrderedItems.Length;
+        // Calculate the total cost.
+        decimal subTotal = new decimal();
+        foreach(OrderedItem oi in OrderedItems)
+        {
+            subTotal += oi.LineTotal;
+        }
+        SubTotal = subTotal;
+        ShipCost = shipCost;
+        TotalCost = SubTotal + ShipCost;
+    }
 }
 
 public class Address
@@ -47,6 +61,7 @@ public class Address
 
 public class OrderedItem
 {
+    public string ItemNumber;
     public string ItemName;
     public string Description;
     public decimal UnitPrice;
@@ -75,10 +90,9 @@ public class Test
     {
         // Creates an instance of the XmlSerializer class;
         // specifies the type of object to serialize.
-        XmlSerializer serializer =
-        new XmlSerializer(typeof(PurchaseOrder));
+        XmlSerializer serializer = new XmlSerializer(typeof(PurchaseOrder));
         TextWriter writer = new StreamWriter(filename);
-        PurchaseOrder po=new PurchaseOrder();
+        PurchaseOrder po = new PurchaseOrder();
 
         // Creates an address to ship and bill to.
         Address billAddress = new Address();
@@ -93,6 +107,7 @@ public class Test
 
         // Creates an OrderedItem.
         OrderedItem i1 = new OrderedItem();
+        i1.ItemNumber = "54100376";
         i1.ItemName = "Widget S";
         i1.Description = "Small widget";
         i1.UnitPrice = (decimal) 5.23;
@@ -101,6 +116,7 @@ public class Test
 
         // Creates an OrderedItem.
         OrderedItem i2 = new OrderedItem();
+        i2.ItemNumber = "54107088";
         i2.ItemName = "Widget T";
         i2.Description = "Tall widget";
         i2.UnitPrice = (decimal) 15.32;
@@ -110,21 +126,13 @@ public class Test
         // Inserts the item into the array.
         OrderedItem [] items = {i1, i2};
         po.OrderedItems = items;
-        // Calculate the total cost.
-        decimal subTotal = new decimal();
-        foreach(OrderedItem oi in items)
-        {
-            subTotal += oi.LineTotal;
-        }
-        po.SubTotal = subTotal;
-        po.ShipCost = (decimal) 12.51;
-        po.TotalCost = po.SubTotal + po.ShipCost;
+        po.Calculate((decimal) 15.21);
         // Serializes the purchase order, and closes the TextWriter.
         serializer.Serialize(writer, po);
         writer.Close();
     }
 
-    protected void ReadPO(string filename)
+    protected PurchaseOrder ReadPO(string filename)
     {
         // Creates an instance of the XmlSerializer class;
         // specifies the type of object to be deserialized.
@@ -132,8 +140,8 @@ public class Test
         // If the XML document has been altered with unknown
         // nodes or attributes, handles them with the
         // UnknownNode and UnknownAttribute events.
-        serializer.UnknownNode+= new XmlNodeEventHandler(serializer_UnknownNode);
-        serializer.UnknownAttribute+= new XmlAttributeEventHandler(serializer_UnknownAttribute);
+        serializer.UnknownNode += new XmlNodeEventHandler(serializer_UnknownNode);
+        serializer.UnknownAttribute += new XmlAttributeEventHandler(serializer_UnknownAttribute);
 
         // A FileStream is needed to read the XML document.
         FileStream fs = new FileStream(filename, FileMode.Open);
@@ -141,31 +149,37 @@ public class Test
         PurchaseOrder po;
         // Uses the Deserialize method to restore the object's state
         // with data from the XML document. */
-        po = (PurchaseOrder) serializer.Deserialize(fs);
+        po = (PurchaseOrder)serializer.Deserialize(fs);
+        DisplayPurchaseOrder(po);
+        return po;
+    }
+
+    private void DisplayPurchaseOrder(PurchaseOrder po)
+    {
         // Reads the order date.
-        Console.WriteLine ("OrderDate: " + po.OrderDate);
+        Console.WriteLine("OrderDate: " + po.OrderDate);
 
         // Reads the shipping address.
         Address shipTo = po.ShipTo;
         ReadAddress(shipTo, "Ship To:");
         // Reads the list of ordered items.
-        OrderedItem [] items = po.OrderedItems;
-        Console.WriteLine("Items to be shipped:");
-        foreach(OrderedItem oi in items)
+        OrderedItem[] items = po.OrderedItems;
+        Console.WriteLine(po.ItemCount + " items to be shipped:");
+        foreach (OrderedItem oi in items)
         {
-            Console.WriteLine("\t"+
-            oi.ItemName + "\t" +
-            oi.Description + "\t" +
-            oi.UnitPrice + "\t" +
-            oi.Quantity + "\t" +
-            oi.LineTotal);
+            Console.WriteLine("\t" +
+                              oi.ItemNumber + ": " + oi.ItemName + "\t" +
+                              oi.Description + "\t" +
+                              oi.UnitPrice + "\t" +
+                              oi.Quantity + "\t" +
+                              oi.LineTotal);
         }
         // Reads the subtotal, shipping cost, and total cost.
         Console.WriteLine(
-        "\n\t\t\t\t\t Subtotal\t" + po.SubTotal +
-        "\n\t\t\t\t\t Shipping\t" + po.ShipCost +
-        "\n\t\t\t\t\t Total\t\t" + po.TotalCost
-        );
+            "\n\t\t\t\t\t\t Subtotal\t" + po.SubTotal +
+            "\n\t\t\t\t\t\t Shipping\t" + po.ShipCost +
+            "\n\t\t\t\t\t\t Total\t\t" + po.TotalCost
+            );
     }
 
     protected void ReadAddress(Address a, string label)
@@ -173,11 +187,11 @@ public class Test
         // Reads the fields of the Address.
         Console.WriteLine(label);
         Console.Write("\t"+
-        a.Name +"\n\t" +
-        a.Line1 +"\n\t" +
-        a.City +"\t" +
-        a.State +"\n\t" +
-        a.Zip +"\n");
+                      a.Name +"\n\t" +
+                      a.Line1 +"\n\t" +
+                      a.City +"\t" +
+                      a.State +"\n\t" +
+                      a.Zip +"\n");
     }
 
     protected void serializer_UnknownNode
@@ -190,7 +204,6 @@ public class Test
     (object sender, XmlAttributeEventArgs e)
     {
         System.Xml.XmlAttribute attr = e.Attr;
-        Console.WriteLine("Unknown attribute " +
-        attr.Name + "='" + attr.Value + "'");
+        Console.WriteLine("Unknown attribute " + attr.Name + "='" + attr.Value + "'");
     }
 }
